c0e808d64c8893fbe80645291a244b2bd58a2f9f
noby-y <noby.official@gmail.com>
2024-02-27 19:15:22 +0200

    fix: entity id selection changed from dev console to CE console

--- aabb_visualizer_add_entity.lua
+++ aabb_visualizer_add_entity.lua
@@ -1,3 +1,6 @@
+if not e or not EntityGetIsAlive(e) then
+    error("No entity in 'e'")
+end
+
 dofile_once("mods/nobys_things/mod_data/scripts/debug/aabb_visualizer.lua")
-entity_id = io.read("*n")
-visualize_entity(entity_id)
+visualize_entity(e)
