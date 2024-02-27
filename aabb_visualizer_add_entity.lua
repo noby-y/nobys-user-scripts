4aeb4b10847208d313460d4308efb718c8163c82
noby-y <noby.official@gmail.com>
2024-02-27 19:10:43 +0200

    add: aabb_visualizer scripts

---
+++ aabb_visualizer_add_entity.lua
@@ -0,0 +1,3 @@
+dofile_once("mods/nobys_things/mod_data/scripts/debug/aabb_visualizer.lua")
+entity_id = io.read("*n")
+visualize_entity(entity_id)
---
+++ aabb_visualizer_add_entity_nearby.lua
@@ -0,0 +1,11 @@
+dofile_once("mods/nobys_things/mod_data/scripts/debug/aabb_visualizer.lua")
+local players = EntityGetWithTag( "player_unit" )
+
+if #players > 0 then
+    local x,y = EntityGetTransform( players[1] )
+    local entities = EntityGetInRadius(x, y, 160)
+    for i,ent in ipairs(entities) do
+        -- print("Entity: " .. ent)
+        visualize_entity(ent) 
+    end 
+end
