aabff350c20793104293b33d883696ed8f09c0ae
noby-y <noby.official@gmail.com>
2024-03-14 03:31:40 +0200

    add: spawn turret on camera pos

--- spawn_debug_turret.lua
+++ spawn_debug_turret.lua
@@ -1,6 +1,10 @@
 local players = EntityGetWithTag( "player_unit" )
-
+local x,y
 if #players > 0 then
-    local x,y = EntityGetTransform( players[1] )
-    EntityLoad("mods/nobys_things/mod_data/entities/buildings/debug_turret.xml", x, y)
+    x,y = EntityGetTransform( players[1] )
+else 
+    x,y = GameGetCameraPos()
 end
+EntityLoad("mods/nobys_things/mod_data/entities/buildings/debug_turret.xml", x, y)
+
+
