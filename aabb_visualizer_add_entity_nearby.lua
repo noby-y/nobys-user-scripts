9de5fe5e84145c60dec5d80ad58ce7c72a97fd98
noby-y <noby.official@gmail.com>
2024-03-19 23:30:58 +0200

    add: check for visualizer comp existance

--- aabb_visualizer_add_entity_nearby.lua
+++ aabb_visualizer_add_entity_nearby.lua
@@ -1,10 +1,23 @@
 dofile_once("mods/nobys_things/mod_data/scripts/debug/aabb_visualizer.lua")
 local players = EntityGetWithTag( "player_unit" )
+local x,y
 
 if #players > 0 then
-    local x,y = EntityGetTransform( players[1] )
-    local entities = EntityGetInRadius(x, y, 160)
-    for i,ent in ipairs(entities) do
-        visualize_entity(ent) 
-    end 
+    x,y = EntityGetTransform( players[1] )
+else 
+    x,y = GameGetCameraPos()
+else
+    return "ERROR: No camera/players found"
 end
+print("x = " .. x .. " y = " .. y)
+local entities = EntityGetInRadius(x, y, 160)
+print("entities = " .. entities)
+for i,ent in ipairs(entities) do
+    local comps = EntityGetAllComponents(entity_id)
+    if ( comps ~= nil ) then
+        for i,comp in ipairs( comps ) do
+            if ( string.find(ComponentGetTypeName(comp), "aabb_visualizer") and ComponentGetIsEnabled(comp)) do
+                visualize_entity(ent) 
+            end
+    end
+end 
