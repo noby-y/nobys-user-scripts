0f5ce38ff31a06d6d613ce8ef53ae91d9af7e75b
noby-y <noby.official@gmail.com>
2024-03-19 23:30:11 +0200

    add: error for no camera/players found

--- spawn_debug_turret.lua
+++ spawn_debug_turret.lua
@@ -1,9 +1,11 @@
 local players = EntityGetWithTag( "player_unit" )
 local x,y
 if #players > 0 then
-    x,y = EntityGetTransform( players[1] )
+    local x,y = EntityGetTransform( players[1] )
 else 
     x,y = GameGetCameraPos()
+else
+    return "ERROR: No camera/players found"
 end
 EntityLoad("mods/nobys_things/mod_data/entities/buildings/debug_turret.xml", x, y)
 
